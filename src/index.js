f317d1b16e8764807535eead5d9e8c1e8e00a32e
Aryan Singh <aryansingh17feb@gmail.com>
2026-04-21 09:52:03 +0530

    updated with cors and cookie-parse code and data base connection

--- src/index.js
+++ src/index.js
@@ -8,7 +8,16 @@ dotenv.config({
     path: "./.env"
 });
 
-connectToDatabase();
+connectToDatabase()
+.then(()=> {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on port ${process.env.PORT}`);
+    })
+})
+.catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+    process.exit(1); // Exit the process with an error code, this is a native node.js method to terminate the process. read more: https://nodejs.org/api/process.html#process_exit_code
+})
 
 
 
